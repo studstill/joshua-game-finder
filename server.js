0843621dca945dbca5a9bb7606d670ed262c6c47
Jason Studstill <standstill64@gmail.com>
2015-07-27 16:25:59 -0700

    Create auth server router

--- server.js
+++ server.js
@@ -2,16 +2,27 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var port = process.env.PORT || 3000;
+var passport = require('passport');
+var expressSession = require('express-session');
+
+app.use(expressSession({secret: 'joshua'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/game_test');
 
 var apiRouter = express.Router();
+var authRouter = express.Router();
 
 require('./routes/users-routes')(apiRouter);
 require('./routes/instance-routes')(apiRouter);
 
-app.use('/api', apiRouter);
+require('./routes/auth-routes')(authRouter);
 
+app.use('/api', require('./routes/verify'));
+app.use('/api', apiRouter);
+app.use('/auth', authRouter);
 
 app.listen(port, function() {
   console.log('Server listening on ' + port);
