d2bd1447945f18be33b3754301b29f26ad8ed5d5
Jason Studstill <standstill64@gmail.com>
2015-07-28 13:29:43 -0700

    Relocated verify.js

--- routes/middleware/verify.js
+++ routes/middleware/verify.js
@@ -3,16 +3,16 @@ var jwt = require('jsonwebtoken');
 module.exports = function(req, res, next) {
   var token = req.body.token || req.query.token || req.headers['x-access-token']
   if (token) {
-    jwt.verify(token, process.env.secret, function(err, decoded) {
+    jwt.verify(token, 'joshua', function(err, decoded) {
       if (err) {
-        res.json({success: false, message: 'Failed to authenticate token.'});
+        res.status(403).json({success: false, message: 'Failed to authenticate token.'});
       } else {
         req.decoded = decoded;
         next();
       }
     });
   } else {
-    res.status(403).send({
+    res.status(403).json({
       success: false,
       msg: 'No token provided'
     });
