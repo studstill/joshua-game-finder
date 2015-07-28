f524e20ab0f1247715170fff40c6c64005854b83
KMSkelton <kmskelton@outlook.com>
2015-07-28 02:07:44 -0700

    GET works for sure. Delete may work (too well)

---
+++ app/js/settings/controllers/instancesController.js
@@ -0,0 +1,87 @@
+'use strict';
+
+module.exports = function(app) {
+  app.controller('instancesController', ['$scope', '$http', function($scope, $http) {
+
+    var getAll = function(){
+      $http.get('/api/instances').success(function(response){
+        console.log(response);
+        $scope.instances = response;
+      });
+    };
+
+    getAll();
+
+    $scope.submitForm = function(instance) {
+      console.log(instance);
+      $http.post('/api/instances', instance).success(function(response) {
+        getAll();
+      });
+    };
+
+    $scope.destroy = function(id) {
+      console.log(id);
+      $http.delete('/api/instances/' + id).success(function(response) {
+        getAll();
+      });
+    }
+
+    $scope.edit = function(instance) {
+      instance.editing = true;
+      console.log(instance);
+    };
+
+    $scope.cancel = function(instance) {
+      getAll();
+    };
+
+    $scope.update = function(instance) {
+      console.log(instance);
+      $http.put('/api/instances/' + instance._id, instance)
+        .error(function (error) {
+          console.log(error);
+          $scope.errors.push({msg: 'could not update instance'});
+        });
+      instance.editing = false;
+      getAll();
+    };
+
+  }]);
+};
+
+
+
+// 'use strict';
+//
+// module.exports = function(app) {
+// 	app.controller('instancesController', ['$scope', 'resource', function($scope, resource) {
+//
+// 		var Instance = resource('instances');
+//
+// 		$scope.getInstances = function(){
+// 			Instance.getAll(function(response){
+// 				console.log(response);
+// 				$scope.instances = response;
+// 			});
+// 		};
+//
+// 		$scope.submitForm = function(instance) {
+// 			console.log('submitted');
+// 			Instance.submitForm(instance, function(response) {
+// 				$scope.getInstances();
+// 			});
+// 		};
+//
+// 		$scope.destroy = function(id) {
+// 			console.log(id);
+// 			Instance.destroy(id, function(response) {
+// 				$scope.getInstances();
+// 			});
+// 		}
+//
+// 		$scope.edit = function(instance) {
+// 			Instance.editing = true;
+// 			console.log(instance);
+// 		};
+// 	}]);
+// };
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var mongoose = require('mongoose');
 var port = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/game_test');
+mongoose.connect(process.env.MONGOLAB_URI);// || 'mongodb://localhost/game_test');
 
 var apiRouter = express.Router();
 
@@ -11,8 +11,10 @@ require('./routes/users-routes')(apiRouter);
 require('./routes/instance-routes')(apiRouter);
 
 app.use('/api', apiRouter);
-
+app.use(express.static(__dirname + '/public')); //tells localhost to look for an HTML file to serve in the '/folder' 
 
 app.listen(port, function() {
   console.log('Server listening on ' + port);
 });
+
+module.exports = app;
