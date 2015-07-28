5f7688e705e8b3a42cc2e3fcc688f9e14788bb4f
Jason Studstill <standstill64@gmail.com>
2015-07-28 16:22:22 -0700

    Implement /user PUT

--- routes/controllers/users-user-controller.js
+++ routes/controllers/users-user-controller.js
@@ -14,6 +14,37 @@ module.exports = {
     });
   },
 
+  put: function(req, res) {
+    var currentUsername = req.params.user;
+    User.findOne({username: currentUsername}, function(err, user) {
+      console.log(currentUsername);
+      console.log(req.decoded.username);
+      if (currentUsername != req.decoded.username) {
+        console.log('I\'m HERE NOW!');
+        res.status(403).json({msg: 'Not allowed'});
+      } else {
+         if (err) {
+          res.status(500).json({msg: 'Server error: ' + err});
+        } else {
+          User.update(user, req.body, function(err, nummAffected) {
+            if (err) {
+              res.status(500).json({msg: 'Server error: ' + err})
+            } else {
+              User.findOne({username: currentUsername}, function(err, user) {
+                if (err) {
+                  res.status(500).json({msg: 'Server error: ' + err})
+                } else {
+                  console.log(user);
+                  res.json({msg: 'Updated: ' + user});
+                }
+              })
+            }
+          })
+        }
+      }
+    });
+  },
+
   delete: function(req, res) {
     User.remove({username: req.params.user}, function(err) {
       if (err) {
