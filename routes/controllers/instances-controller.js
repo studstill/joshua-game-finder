9a350ac2c527dec3bfb7f4b814a24c1600f4822d
Nick Kuhn <nickuhn@gmail.comn>
2015-07-29 13:51:25 -0700

    reformatted responses

--- routes/controllers/instances-controller.js
+++ routes/controllers/instances-controller.js
@@ -1,14 +1,15 @@
 var mongoose = require('mongoose');
 var Instance = require(__dirname + '/../../models/Instance.js');
+var User = require(__dirname + '/../../models/User.js');
 
 module.exports = {
 
   get: function(req, res) {
     Instance.find({}, function(err, data) {
       if (err) {
-        res.send(err);
+        res.status(500).json({success: false, msg: 'Error finding instances', error: err});
       } else {
-        res.send(data);
+        res.json({success: true, msg: 'Get all instances successful', data: data});
       }
     });
   },
@@ -17,15 +18,16 @@ module.exports = {
     var instance = new Instance(req.body);
     instance.creator = req.decoded._id;
     User.findOneAndUpdate({_id: req.decoded._id}, {hosting: true,
-      isCommitted: true}, function(err, numAffected) {
-        if (err) throw err;
-        console.log('Number affected: ' + numAffected);
+      isCommitted: true}, function(err, numchanged) {
+        if (err) {
+          res.status(500).json({success: false, msg: 'Error finding host user', error: err});
+        }
     });
     instance.save(function(err, data) {
       if (err) {
-        res.send(err);
+        res.status(500).json({success: false, msg: 'Error saving instance', error: err});
       } else {
-        res.json(data);
+        res.json({success: true, msg: 'Instance created successfully', data: data});;
       }
     });
 
