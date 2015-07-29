472fd659092e832466ff95cb970d24cb4e0b3ac9
Nick Kuhn <nickuhn@gmail.comn>
2015-07-29 15:06:15 -0700

    readded verify to routes

--- routes/instance-routes.js
+++ routes/instance-routes.js
@@ -1,6 +1,6 @@
 var instancesController = require('./controllers/instances-controller');
 var instancesInstanceController = require('./controllers/instances-instance-controller');
-
+var verify = require('./middleware/verify');
 var bodyParser = require('body-parser');
 var jwt = require('jsonwebtoken');
 
@@ -9,7 +9,7 @@ module.exports = function(router) {
   router.use(bodyParser.json());
 
   router.get('/instances', instancesController.get)
-  router.post('/instances', instancesController.post)
+  router.post('/instances', verify, instancesController.post)
 
   router.get('/instances/:instance', instancesInstanceController.get)
 
